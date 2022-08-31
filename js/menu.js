8d2c09384b6401a82c678fabad8d26ed39f8a56c
Maksym Kundeev <mkundeev@gmail.com>
2022-08-31 18:33:01 +0300

    done

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-menu-open]"),
+    closeModalBtn: document.querySelector("[data-menu-close]"),
+    modal: document.querySelector("[data-menu]"),
+    backDrop: document.querySelector("[data-back-drop]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-closed");
+    refs.backDrop.classList.toggle("d-none");
+  }
+})();
